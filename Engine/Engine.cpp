69480bdc3474dbac036eedb717a323bccd94addc
mkinneyperez <mkinneyperez@CHIEFKINNEY>
2026-06-30 20:31:42 -0600

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "framework.h"
+#include "Engine.h"
+#include <iostream>
+
+// TODO: This is an example of a library function
+void fnEngine() {
+	std::cout << "Welcome to the Library" << std::endl;
+	return;
+}
